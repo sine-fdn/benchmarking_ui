9390e59efbfb6af67f5584de12547c708da260e1
Martin Pompéry <ms@funkpopes.org>
2021-03-24 13:53:46 +0100

    coordinator-level most-simple load balancing

--- src/api_lib/nextCoordinator.ts
+++ src/api_lib/nextCoordinator.ts
@@ -1,10 +1,23 @@
 import { QueueKind } from "@prisma/client";
 import { CoordinatorUrl } from "@sine-fdn/sine-ts";
+import prismaConnection from "../utils/prismaConnection";
 
-export default function nextCoordinator(
-  _: QueueKind,
+export default async function nextCoordinator(
+  qkind: QueueKind,
   coordinators: string[]
-): CoordinatorUrl {
-  //const idx = Math.floor(Math.random() * coordinators.length);
-  return coordinators[0];
+): Promise<CoordinatorUrl> {
+  if (qkind !== QueueKind.DATASET || coordinators.length < 2) {
+    return coordinators[0];
+  }
+
+  const { count } = await prismaConnection().processingQueue.aggregate({
+    count: true,
+    where: {
+      qkind,
+    },
+  });
+
+  // dedicate the first coordinator for non-dataset calls
+  const coordinator = coordinators[1 + (count % (coordinators.length - 1))];
+  return coordinator;
 }
